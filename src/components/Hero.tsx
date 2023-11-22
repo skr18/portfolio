d20cf62fa80d59fc93bdd13f620d2c2f5635dc7c
Sujeet Kumar Rath <93946478+skr18@users.noreply.github.com>
2023-11-23 01:39:48 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -21,7 +21,9 @@ function Hero({}: Props) {
     <div className='h-screen flex flex-col space-y-15 items-center justify-center text-center overflow-hidden'>
         <BackgroundCircle/>
         <img className='rounded-full relative h-32 w-32 mx-auto' 
-        src="https://i.postimg.cc/gJb14fht/profile-photo.jpg" alt="profileImage" />
+        src="https://i.postimg.cc/gJb14fht/profile-photo.jpg" alt="profileImage" style="
+                width: 12%;
+                height: 24%;" />
         <div className='z-20'>
             <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]' >
                 Software Engineer
